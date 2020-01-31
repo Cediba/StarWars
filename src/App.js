e3b6df4d3bfc9750563767d2b56d91c60c093264
Cedric <cedric.balmer@gmx.ch>
2020-01-31 13:47:55 -0800

    set up APi

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [people, setPeople] = useState([]);
+  const [planets, setPlanets] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+      async function fetchPeople() {
+        let res = await fetch('https://swapi.co/api/people/?format=json');
+        let data  = await res.json();
+        setPeople(data.results);
+      }
+
+      async function fetchPlanets(){
+        let res = await fetch('https://swapi.co/api/planets/?format=json');
+        let data = await res.json();
+        setPlanets(data.results);
+      }
+
+      fetchPeople();
+      fetchPlanets();
+      
+    }, [])
+    console.log('people', people);
+    console.log('planets', planets);
+    return (
+      <div className='App'>
+        Hello 
+      </div>
   );
 }
 
